48306e87317a0d502bcbf9560c92da1fde8d76a0
Max Morris <mmor115@lsu.edu>
2023-12-27 17:39:22 -0600

    Commit 3.

--- CppParallelSuite/KeyType.hpp
+++ CppParallelSuite/KeyType.hpp
@@ -7,10 +7,13 @@
 #include "Types.hpp"
 
 namespace parallel_suite {
-    template<typename K>
-    concept KeyType = std::totally_ordered<K> && requires(K k) {
+    template <typename K>
+    concept KeyType = std::equality_comparable<K> && requires(K k) {
         { std::hash<K>{}(k) } -> std::convertible_to<usize>;
     };
+
+    template <typename K>
+    concept OrderedKeyType = std::totally_ordered<K> && KeyType<K>;
 }
 
 #endif // KEYTYPE_HPP
