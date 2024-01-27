e30386d48a735db5f0abe7675ee7825f2aef16a1
Steven R. Brandt <sbrandt@cct.lsu.edu>
2024-01-26 21:15:21 -0600

    Fix cmake build problems, remove refs to deleted test, and add TwoCounterLock.

--- JavaParallelSuite/src/edu/lsu/cct/parallelsuite/bench/TestMaps.java
+++ JavaParallelSuite/src/edu/lsu/cct/parallelsuite/bench/TestMaps.java
@@ -1,6 +1,5 @@
 package edu.lsu.cct.parallelsuite.bench;
 
-import edu.lsu.cct.parallelsuite.maps.LockHashMap;
 import edu.lsu.cct.parallelsuite.maps.SetBasedMap;
 import edu.lsu.cct.parallelsuite.sets.FineGrainedSet;
 import edu.lsu.cct.parallelsuite.sets.LazySet;
@@ -92,7 +91,6 @@ public class TestMaps {
         // Sanity check: Standard Java implementation
         testMap(ConcurrentHashMap::new);
 
-        testMap(LockHashMap::new);
         testMap(() -> new SetBasedMap<>(FineGrainedSet::new));
         testMap(() -> new SetBasedMap<>(OptimisticSet::new));
         testMap(() -> new SetBasedMap<>(LazySet::new));
---
+++ JavaParallelSuite/src/edu/lsu/cct/parallelsuite/locks/TwoCounterLock.java
@@ -0,0 +1,26 @@
+package edu.lsu.cct.parallelsuite.locks;
+
+import edu.lsu.cct.parallelsuite.ThreadId;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TwoCounterLock implements SlimLock {
+    private final AtomicInteger currentTicket = new AtomicInteger(0);
+    private final AtomicInteger ticketDispenser = new AtomicInteger(0);
+
+    @Override
+    public void lock() {
+        var myTicket = ticketDispenser.getAndIncrement();
+
+        // wait for my ticket to come up
+        while(currentTicket.get() != myTicket) {
+            Thread.yield();
+        }
+    }
+
+    @Override
+    public void unlock() {
+        // advance to the next ticket
+        currentTicket.getAndIncrement();
+    }
+}
