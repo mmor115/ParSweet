6389e6246a2cef95ab1a54e2ae0fef685d8f184b
Max Morris <mmor115@lsu.edu>
2024-02-05 13:53:44 -0600

    Add std and hpx locks to BenchSetByLocks

--- CppParallelSuite/bench/BenchSetByLocks.cpp
+++ CppParallelSuite/bench/BenchSetByLocks.cpp
@@ -2,7 +2,10 @@
 
 #include <vector>
 #include <future>
-
+#if HAVE_HPX
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_main.hpp>
+#endif
 #include "../sets/FineGrainedSet.hpp"
 #include "../locks/LockTraits.hpp"
 #include "../locks/ALock.hpp"
@@ -17,6 +20,29 @@
 #include "../locks/TwoCounterLock.hpp"
 
 #include "BlackBox.hpp"
+
+namespace parallel_suite::locks {
+    template <>
+    struct LockTraits<std::mutex> {
+        constexpr static char const* name = "std::mutex";
+    };
+
+    template <>
+    struct LockTraits<std::recursive_mutex> {
+        constexpr static char const* name = "std::recursive_mutex";
+    };
+#if HAVE_HPX
+    template <>
+    struct LockTraits<hpx::mutex> {
+        constexpr static char const* name = "hpx::mutex";
+    };
+
+    template <>
+    struct LockTraits<hpx::spinlock> {
+        constexpr static char const* name = "hpx::spinlock";
+    };
+#endif
+}
 
 namespace parallel_bench::sets {
     using namespace parallel_suite;
@@ -91,6 +117,8 @@ int main() {
     BenchParameters params("c++", "setByLocks");
 
     benchSets<FineGrainedSet,
+              std::mutex,
+              std::recursive_mutex,
               locks::ALock<100>,
               locks::BackoffLock<1, 17>,
               locks::CLHLock,
@@ -101,6 +129,10 @@ int main() {
               locks::TIdLock,
               locks::TTASLock,
               locks::TwoCounterLock
+#if HAVE_HPX
+,hpx::mutex
+,hpx::spinlock
+#endif
     >(params, "FineGrainedSet", params.getWhich());
 
     return 0;
